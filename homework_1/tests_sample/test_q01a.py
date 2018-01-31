684f1377b4485859450d24959d2d43fe7a98b683
Tanveer Shaikh <tshaik4@uic.edu>
2018-01-30 23:34:40 -0600

    Add files via upload

--- homework_1/tests_sample/test_q01a.py
+++ homework_1/tests_sample/test_q01a.py
@@ -1,6 +1,7 @@
 import unittest
 from hw1 import prime_nums_reversed
-
+from hw1 import string_explosion
+from hw1 import consecutive
 
 class TestQ01A(unittest.TestCase):
     def setUp(self):
@@ -9,3 +10,31 @@ class TestQ01A(unittest.TestCase):
     def test_nums_one(self):
         """Evaluate prime_nums_reversed(1)"""
         self.assertEqual(prime_nums_reversed(1), '')
+        self.assertEqual(prime_nums_reversed(5), '5 3 2')
+    
+    def test_nums_other(self):
+        self.assertEqual(prime_nums_reversed(5), '5 3 2')
+        self.assertEqual(prime_nums_reversed(10), '7 5 3 2')
+        self.assertEqual(prime_nums_reversed(50), '47 43 41 37 31 29 23 19 17 13 11 7 5 3 2')
+        self.assertEqual(prime_nums_reversed(100), '97 89 83 79 73 71 67 61 59 53 47 43 41 37 31 29 23 19 17 13 11 7 5 3 2')
+
+    def test_string_explosion(self):
+        self.assertEqual(string_explosion(''), '')
+        self.assertEqual(string_explosion('   '), '      ')
+        self.assertEqual(string_explosion(None), '')
+        self.assertEqual(string_explosion('Code'), 'Codeodedee')
+        self.assertEqual(string_explosion('data!'), 'data!ata!ta!a!!')
+        self.assertEqual(string_explosion('hi'), 'hii')
+        self.assertEqual(string_explosion('h'), 'h')
+        
+    def test_consecutive(self):
+        """Evaluate prime_nums_reversed(1)"""
+        self.assertEqual(consecutive([50, 34, 3, 15]), False)
+        self.assertEqual(consecutive([7, 3, 20, 21, 8]), True)
+        self.assertEqual(consecutive([-41,40]), False)
+        self.assertEqual(consecutive([-1,0]), True)
+        self.assertEqual(consecutive([-41,-40]), True)
+        self.assertEqual(consecutive([]), False)
+        self.assertEquals(consecutive([-1, 0, 3, 4]), True)
+        self.assertEquals(consecutive([-1, 2, -4]), False)
+        
--- homework_1/tests_sample/test_q02a.py
+++ homework_1/tests_sample/test_q02a.py
@@ -1,7 +1,8 @@
 import unittest
 import numpy as np
 from hw1 import bowl_cost
-
+from hw1 import amount_spent
+from hw1 import new_price
 
 class TestQ02A(unittest.TestCase):
     def setUp(self):
@@ -10,3 +11,13 @@ class TestQ02A(unittest.TestCase):
     def test_one_three_five(self):
         """Evaluate bowl_cost([1, 3, 5])"""
         self.assertTrue(np.array_equal(bowl_cost([1, 3, 5]), np.array([27, 42, 30, 50])))
+        
+    def test_def_amount_spent(self):
+        v = np.array([1,3,5])
+        B = np.array([
+        [3,3,3],
+        [2,0,8],
+        [0,5,3],
+        [0,0,10]
+        ])
+        self.assertTrue(np.array_equal(amount_spent(v,B), np.array([84, 149, 500])))
